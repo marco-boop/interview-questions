ec0543bc4fade80a43d2b8cc125da9907cb9736c
Marco <sanchez.junco@gmail.com>
2024-01-08 10:42:12 -0800

    make it recursive

--- 1436-destination-city.js
+++ 1436-destination-city.js
@@ -1,23 +1,51 @@
-const paths = [["London", "New York"], ["New York", "Lima"], ["Lima", "Sao Paulo"]]
+const paths = [["London", "New York"], ["Lima", "Sao Paulo"], ["New York", "Lima"]]
 const paths2 = [["B", "C"], ["D", "B"], ["C", "A"]]
 const paths3 = [["A", "Z"]]
 
-let pathedcities = [];
+function destinationCity(paths, i = 0) {
+    if (i >= paths.length) return;
 
-for (let i = 0; i < paths.length; i++) {
-    pathedcities.push(paths[i][0])
+    for (let j = 0; j < paths.length; j++) {
+        // console.log("Comparing ", paths[i][1], " to ", paths[j][0]);
+        if (paths[i][1] === paths[j][0]) {
+            return destinationCity(paths, i + 1);
+        }
+        else {
+            // console.log("Still comparing");
+            // console.log("Array position is ", "i ", i, "j ", j);
+            if (j === paths.length - 1) {
+                // console.log("We reached end of array and did not find city");
+                return paths[i][1];
+            }
+        }
+    }
 }
 
-for (j = 0; j < paths.length; j++) {
-    if (pathedcities.includes(paths[j][1])) {
+const answer = destinationCity(paths);
+console.log(answer);
 
-    }
-    else {
-        console.log(paths[j][1]);
-    }
-}
 
-//Below works but trying something new
+
+
+//2 - Below works but trying something new
+
+
+// let pathedcities = [];
+
+// for (let i = 0; i < paths.length; i++) {
+//     pathedcities.push(paths[i][0])
+// }
+
+// for (j = 0; j < paths.length; j++) {
+//     if (pathedcities.includes(paths[j][1])) {
+
+//     }
+//     else {
+//         console.log(paths[j][1]);
+//     }
+// }
+
+//1 - Below works but trying something new
 
 // let allcities = [];
 // let pathedcities = [];
