c607d7691ff61e0ea32ad2ab1a52bd86a2dda058
Marco <sanchez.junco@gmail.com>
2024-01-08 09:53:18 -0800

    recursive fn answer

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,15 +1,30 @@
 const nums = [2, 7, 11, 15];
 const target = 9
 
-for (let i = 0; i < nums.length; i++) {
-    // console.log("Position: ", i, "Value: ", nums[i]);
+// for (let i = 0; i < nums.length; i++) {
+//     // console.log("Position: ", i, "Value: ", nums[i]);
+//     for (let j = 0; j < nums.length; j++) {
+//         // console.log(nums[i] + nums[j]);
+//         if (i !== j) {
+//             if (nums[i] + nums[j] == target) {
+//                 console.log(i, j);
+//                 return [i, j];
+//             }
+//         }
+//     }
+// }
+
+function twoSums(nums, target, i = 0) {
+    if (i >= nums.length) return;
+
     for (let j = 0; j < nums.length; j++) {
-        // console.log(nums[i] + nums[j]);
-        if (i !== j) {
-            if (nums[i] + nums[j] == target) {
-                console.log(i, j);
-                return [i, j];
-            }
+        if (i !== j && nums[i] + nums[j] === target) {
+            return [i, j];
         }
     }
+
+    return twoSums(nums, target, i + 1);
 }
+
+const answer = twoSums(nums, target);
+console.log(answer);
