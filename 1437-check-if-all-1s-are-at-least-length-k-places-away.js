a21cd7daf6051bf74a85ca97c8befd2b8dc633f6
Marco <sanchez.junco@gmail.com>
2024-01-08 16:21:07 -0800

    Testcases passed, but took too long.

--- 1437-check-if-all-1s-are-at-least-length-k-places-away.js
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away.js
@@ -1,31 +1,24 @@
-const nums = [1, 0, 0, 0, 1, 0, 0, 1];
-const k = 2;
+const nums = [1, 1, 1, 1, 1, 1, 1, 1, 1,];
+const k = 0;
 
 function distance(nums, k, i = 0) {
 
+    if (k === 0) return true;
     if (i >= nums.length - 1) return true;
 
-    for (let j = 1; j < nums.length; j++) {
-        if (i == j) {
-            j = j + 1;
-        }
-        else {
-            if (nums[i] == 1 && nums[j] == 1) {
-                if (j - i < k) {
-                    return false;
-                }
-                if (j - i >= k) {
-                    console.log("Distance is ", j - i - 1);
-                    i = i + 1;
-                }
+
+    for (let j = i + 1; j < nums.length; j++) {
+        if (nums[i] == 1 && nums[j] == 1) {
+            console.log("j minus i is ", j - i);
+            if (j - i - 1 < k) {
+                console.log(j - i - 1, " is less than ", k);
+                return false;
             }
         }
     }
-
     if (i < nums.length - 1) {
         return distance(nums, k, i + 1);
     }
-
     return true;
 };
 
