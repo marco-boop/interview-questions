48696ad86f9c4f802de81401306092c92dbbf037
Marco <sanchez.junco@gmail.com>
2024-01-09 11:07:28 -0800

    Clean up

--- 1437-check-if-all-1s-are-at-least-length-k-places-away.js
+++ 1437-check-if-all-1s-are-at-least-length-k-places-away.js
@@ -1,5 +1,5 @@
 const nums = [1, 0, 0, 0, 1, 0, 0, 1];
-const k = 5;
+const k = 2;
 
 function distance(nums, k, valid = true) {
 
@@ -12,11 +12,7 @@ function distance(nums, k, valid = true) {
     };
 
     for (let j = 1; j < onesOnly.length; j++) {
-        if (((onesOnly[j] - onesOnly[j - 1]) - 1) >= k) {
-            console.log("Distance larger or equal: ", (onesOnly[j] - onesOnly[j - 1]) - 1);
-        }
-        else {
-            console.log("Valid is now false")
+        if (((onesOnly[j] - onesOnly[j - 1]) - 1) < k) {
             valid = false;
         }
     };
@@ -25,42 +21,3 @@ function distance(nums, k, valid = true) {
 };
 const answer = distance(nums, k);
 console.log(answer);
-
-// if (k === 0) return true;
-// if (i >= nums.length - 1) return true;
-
-// for (let j = i + 1; j < nums.length; j++) {
-//     if (nums[i] == 1 && nums[j] == 1) {
-//         console.log("j minus i is ", j - i);
-//         if (j - i - 1 < k) {
-//             console.log(j - i - 1, " is less than ", k);
-//             return false;
-//         }
-//     }
-// }
-// if (i < nums.length - 1) {
-//     return distance(nums, k, i + 1);
-// }
-// return true;
-
-
-
-// function distance(nums, k, i = 0, valid = true) {
-
-//     if (k === 0) return true;
-//     if (i >= nums.length - 1) return true;
-
-//     for (let j = i + 1; j < nums.length; j++) {
-//         if (nums[i] == 1 && nums[j] == 1) {
-//             console.log("j minus i is ", j - i);
-//             if (j - i - 1 < k) {
-//                 console.log(j - i - 1, " is less than ", k);
-//                 return false;
-//             }
-//         }
-//     }
-//     if (i < nums.length - 1) {
-//         return distance(nums, k, i + 1);
-//     }
-//     return true;
-// };
